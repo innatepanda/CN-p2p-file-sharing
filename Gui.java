92fa465f07fe75502ce0d178571398a4482a2939
virtualpanda <1905228@kiit.ac.in>
2021-11-08 00:26:50 +0530

    second panel added

--- Gui.java
+++ Gui.java
@@ -4,25 +4,54 @@ import java.awt.event.*;
 public class Gui extends JFrame implements ActionListener{
     JTextField t1,t2,t3; 
     JButton b;  
-    JLabel l1;
+    JLabel l1,l2,l3,l4;
     JPanel p, q;
     CardLayout crd;    
     JPanel cPane;  
     Gui(){
-        //cPane = getContentPane(); 
-        cPane=new JPanel();
-        setLayout(crd);
+        cPane =new JPanel();  
         crd = new CardLayout();    
         cPane.setLayout(crd);  
-        
         p = new JPanel();
+        q = new JPanel();
         p.setLayout(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
-            gbc.insets = new Insets(1, 8, 8, 1);
+            gbc.insets = new Insets(8, 8, 8, 8);
             gbc.gridx = 0;
             gbc.gridy = 0;
         p.setBounds(0,0,500,500);
-        //p.setBackground(Color.black);
+        p.setBackground(Color.white);
+          
+        t1=new JTextField(20);  
+        
+        t2=new JTextField(20); 
+        
+        l1=new JLabel();l2=new JLabel();l3=new JLabel();l4=new JLabel();
+        l1.setFont (l1.getFont ().deriveFont (30.0f));
+        l1.setText("Login");  l2.setText("Username");  l3.setText("Password");
+        //p.add(Box.createRigidArea(new Dimension(0,5)));
+        gbc.gridx=1;  p.add(l1, gbc);
+        gbc.gridx=0;
+
+        gbc.gridy++;
+        p.add(l2, gbc);
+        gbc.gridx++;
+        p.add(t1, gbc);
+
+        gbc.gridy++;
+        gbc.gridx=0;
+        p.add(l3, gbc);
+        
+        gbc.gridx++;
+        p.add(t2, gbc);
+        b =new JButton("click");//creating instance of JButton  
+        
+        b.addActionListener(this); 
+        gbc.gridy++;
+        p.add(b, gbc);
+        gbc.gridy++;
+        p.add(l4, gbc);
+
         q = new JPanel();
         q.setLayout(new GridBagLayout());
         GridBagConstraints qgbc = new GridBagConstraints();
@@ -32,30 +61,10 @@ public class Gui extends JFrame implements ActionListener{
         q.setBounds(0,0,500,500);
         q.setBackground(Color.black);
         
-        
-        t1=new JTextField(20);  
-        
-        t2=new JTextField(20); 
-        
-        l1=new JLabel();  
-        l1.setText("Login");
-        
-        p.add(l1, gbc);
-        //p.add(Box.createRigidArea(new Dimension(0,5)));
-        gbc.gridy++;
-        p.add(t1, gbc);
-        gbc.gridy++;
-        p.add(t2, gbc);
-        b =new JButton("click");//creating instance of JButton  
-        
-        b.addActionListener(this); 
-        gbc.gridy++;
-        p.add(b, gbc);
-    // cPane.add(l1);
         cPane.add(p, "a");
         cPane.add(q, "b");  
         setContentPane(cPane); 
-
+             
             
     }
     public void actionPerformed(ActionEvent e) 
@@ -65,16 +74,15 @@ public class Gui extends JFrame implements ActionListener{
         String s2=t2.getText();  
         
         if(e.getSource()==b){  
-            l1.setText("usr, pass "+s1+" is: "+s2);
-            
-            crd.next(cPane);
+            l4.setText("usr , pass of "+s1+" is: "+s2);
+            crd.next(cPane);  
         }
     }
     
        public static void main(String[] args) {  
           Gui g=new Gui();
           g.setSize(500,500);  
-          
+          //g.setLayout();  
           g.setVisible(true);
           g.setDefaultCloseOperation(EXIT_ON_CLOSE);    
        } 
